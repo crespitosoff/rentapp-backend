f05c90755b6068c8d09ddea17b074d6cb7e6c2ba
Sebastian Montealegre <jhoseb2006.com@gmail.com>
2025-10-02 21:22:06 -0500

    conexión con base de datos remota

--- src/services/db.js
+++ src/services/db.js
@@ -5,14 +5,17 @@ const { Pool } = require('pg');
 // Crea una nueva instancia de Pool con los datos de conexión.
 // Tu aplicación usará este 'pool' para enviar consultas a PostgreSQL.
 const pool = new Pool({
-  user: 'postgres',                  // El usuario por defecto de PostgreSQL
-  host: 'localhost',                 // La dirección del servidor de la base de datos (tu máquina)
-  database: 'rentapp_db',            // El nombre de la base de datos que creamos
-  password: 'Cr3sp0s', // La contraseña que estableciste al instalar
-  port: 5432,                        // El puerto por defecto de PostgreSQL
+  user: 'postgres.zrhcddrhgcqnhvxjavug',                  // El usuario por defecto de PostgreSQL
+  host: 'aws-1-sa-east-1.pooler.supabase.com',                 // La dirección del servidor de la base de datos (tu máquina)
+  database: 'postgres',            // El nombre de la base de datos que creamos
+  password: 'Cr3p0s&T0m4s', // La contraseña que estableciste al instalar
+  port: 6543,                        // El puerto por defecto de PostgreSQL
+  // AÑADIDO: Configuración para requerir una conexión segura (SSL)
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // Exporta la instancia de 'pool' para que otros archivos de tu aplicación
 // puedan usarla para interactuar con la base de datos.
-module.exports = po
-ol;
+module.exports = pool;
